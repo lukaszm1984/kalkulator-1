9094a707ce6afe43fb5a8cdfdc419fd29404cd9a
Grzegorz <grzegorz.zaw@live.com>
2019-09-28 19:06:24 +0200

    Add menu to program

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -12,7 +12,13 @@ namespace ConsoleApplication1
         {
             while (true)
             {
-
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. ADD");
+                Console.WriteLine("2. DIFF");
+                int menuOption = Convert.ToInt32(Console.ReadLine());
+                if (menuOption == 0) {
+                    break;
+                }
             }
         }
     }
